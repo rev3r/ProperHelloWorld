a96438c0f80496eb7c1761667b17e401f9d300b0
Grzegorz Lang <rev3r@outlook.com>
2018-07-30 17:52:02 +0200

    Add integration test

---
+++ tests/IntegrationTests/GetWelcomeText.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using FakeItEasy;
+using Infrastructure.Data;
+using Infrastructure.TextRepository;
+using Xunit;
+
+namespace IntegrationTests
+{
+	public class GetWelcomeText
+	{
+		private readonly ITextRepository repository;
+
+		public GetWelcomeText()
+		{
+			var mapper = A.Fake<IMapper>();
+			repository = new TextRepository(mapper);
+
+			A.CallTo(() => mapper.Map<TextEntity>(A<TextData>.That.Matches(td => td.Text == "Hello World!")))
+				.Returns(new TextEntity("Hello World!"));
+		}
+
+		[Fact]
+		public void Invokation_RetrieveValidWelcomeText()
+		{
+			var result = repository.GetWelcomeText();
+
+			Assert.Equal("Hello World!", result.Text);
+		}
+	}
+}
