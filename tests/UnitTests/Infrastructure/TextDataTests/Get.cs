4dcfcf26efa15a2884d688f40e97da7dee3e2fe0
Grzegorz Lang <rev3r@outlook.com>
2018-08-18 23:21:31 +0200

    Create TextData test

---
+++ tests/UnitTests/Infrastructure/TextDataTests/Get.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Data;
+using Xunit;
+
+namespace UnitTests.Infrastructure.TextDataTests
+{
+	public class Get
+	{
+		private readonly TextData data;
+
+		public Get()
+		{
+			data = new TextData();
+		}
+
+		[Fact]
+		public void GetValue_ReturnsText()
+		{
+			Assert.Equal("Hello World!", data.Text);
+		}
+	}
+}
